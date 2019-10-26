2019d86588e2fc9920fdc12d6ad01add57c0af23
Enrico Schumann <es@enricoschumann.net>
2019-10-26 14:03:32 +0200

    Use 'iconv' to remove invalid bytes

--- R/SSRN_stats.R
+++ R/SSRN_stats.R
@@ -1,6 +1,3 @@
-## id <- 372409
-## id <- 895233
-
 SSRN_stats <- function(id, write.file = FALSE,
                        append.file = TRUE,
                        file.prefix = "SSRN",
@@ -10,7 +7,8 @@ SSRN_stats <- function(id, write.file = FALSE,
                     "cf_dev/AbsByAuth.cfm?per_id=", id))
     txt <- readLines(u, warn = FALSE, encoding = "UTF-8")
     close(u)
-    
+    txt <- iconv(txt, from = "UTF-8", to = "UTF-8")
+
     txt_ <- paste(txt, collapse = "")
     pos <- gregexpr("<a class=\"title optClickTitle\" href=\"https://ssrn.com/abstract=",
                     txt_)
@@ -22,7 +20,7 @@ SSRN_stats <- function(id, write.file = FALSE,
                          stringsAsFactors = FALSE)
     for (i in seq_len(length(pos) - 1)) {
         txt0 <- substr(txt_, pos[i], pos[i+1])
-        
+
         i_id <- as.numeric(gsub(".*https://ssrn.com/abstract=([0-9]+).*", "\\1", txt0))
         i_title <- sub(paste0("<a class=\"title optClickTitle\" href=\"https://ssrn.com/abstract=",
                               ".*?<span>(.*?)</span>.*"), "\\1", txt0)
@@ -31,19 +29,19 @@ SSRN_stats <- function(id, write.file = FALSE,
         papers[["Id"]][i] <- i_id
         papers[["Title"]][i] <- i_title
         papers[["Downloads"]][i] <- i_dl
-        
+
     }
 
     papers[["Downloads"]] <- as.numeric(gsub(",", "", papers[["Downloads"]], fixed = TRUE))
 
-    
+
     ## overall rank; two backreferences: rank by downloads, by citations
     tmp <- gsub(".*<p>SSRN RANKINGS</p>.*?<h4>Top(.*?)</h4>.*<p>SSRN RANKINGS</p>.*?<h4>Top(.*?)</h4>.*",
          "\\1---\\2", txt_)
 
     tmp <- strsplit(tmp, "---", fixed = TRUE)[[1L]]
     rank <- as.numeric(gsub(",", "", tmp, fixed = TRUE))
-        
+
     if (write.file && NROW(papers)) {
         for (i in seq_len(NROW(papers)))
             cat(as.character(Sys.time()), ";", papers[["Downloads"]][[i]], ";\n",
@@ -54,5 +52,5 @@ SSRN_stats <- function(id, write.file = FALSE,
          author.rank.citations = rank[[2L]],
          total.downloads = sum(papers[["Downloads"]]),
          downloads = papers)
-    
+
 }
